7248321863207cf5b17551e7f0adbd61fe91465b
JokeyChen <jokey.chen@gmail.com>
2016-03-11 15:59:20 -0800

    Prompt user for url and interval

---
+++ gs_checker.py
@@ -0,0 +1,13 @@
+def main():
+    url = raw_input('Please specify the GradeSource website you want to check:')
+    if len(url) == 0:
+        print 'Empty url. Program aborts.'
+        return
+    try:
+        interval = int(raw_input('Please specify the checking interval (in minute):'))
+    except ValueError:
+        print 'Invalid interval. Program aborts.'
+        return]
+
+if __name__ == '__main__':
+    main()
