f0733ae24d39636b5077609f0bae9ebeacb67d98
JokeyChen <jokey.chen@gmail.com>
2016-03-11 16:01:34 -0800

    Fix typo

--- gs_checker.py
+++ gs_checker.py
@@ -7,7 +7,7 @@ def main():
         interval = int(raw_input('Please specify the checking interval (in minute):'))
     except ValueError:
         print 'Invalid interval. Program aborts.'
-        return]
+        return
 
 if __name__ == '__main__':
     main()
