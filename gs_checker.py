ba9d58dc48512d7f2ec274290f1e04b8ac6534bf
JokeyChen <jokey.chen@gmail.com>
2016-03-12 13:20:51 -0800

    Redesign

--- gs_checker.py
+++ gs_checker.py
@@ -1,15 +1,111 @@
-def main():
-    url = raw_input('Please specify the GradeSource website you want to check:')
-    if len(url) == 0:
-        print 'Empty url. Program aborts.'
-        return
+#!/usr/bin/python
+import time
+import urllib
+import smtplib
+from bs4 import BeautifulSoup
+
+URL = 'SPECIFY GRADE SOURCE URL HERE'
+SENDER = 'SPECIFY EMAIL SENDER HERE'
+RECEIVERS = ['SPECIFY EMAIL RECEIVERS HERE']
+MSG = """From: John Doe <johndoe@xxx.com>
+To: John Doe <johndoe@yyy.com>
+Subject: GradeSource Update
+
+GradeSource Update Detected!
+FEEL FREE TO MODIFY THE EMAIL MESSAGE HERE
+"""
+SMTP_SERVER = 'SPECIFY THE SMTP SERVER HERE. DO NOT FORGET ITS PORT BELOW'
+SMTP_PORT = 587
+MAIL_LOGIN = 'SPECIFY LOGIN FOR SMTP'
+MAIL_PW = 'SPECIFY PASSWORD FOR SMTP HERE'
+
+
+FILE_READ_MODE = 'r'
+FILE_WRITE_MODE = 'w'
+
+CHAR_NEWLINE = '\n'
+TIME_STR_FORMAT = '%Y-%m-%d %H:%M:%S' # Feel free to modify the format
+PARSER = 'html.parser'
+
+LAST_UPDATE = 'ref/lastupdate' # this file must be present prior to running
+LAST_CHECK =  'ref/lastcheck'
+
+STR_UPDATE = 'Updated! Time is'
+STR_NO_UPDATE = 'No update at'
+STR_ERR_SEND_EMAIL = 'Error trying to send email!'
+
+
+
+def get_current_time_str():
+    """
+    """
+    return time.strftime(TIME_STR_FORMAT, time.localtime())
+
+def get_webpage():
+    """
+    """
+    content = urllib.urlopen(URL).read()
+    return BeautifulSoup(content, PARSER)
+
+def find_update(soup):
+    """
+    """
+    # TODO: need to come up with a more generic way to find the update string
+    update = soup.find_all('font')
+    return (update[4].contents[1])[1:] + '\n'
+
+def check_diff(time_on_gs):
+    """
+    """
+    lastupdate = open(LAST_UPDATE, FILE_READ_MODE)
+    # TODO: Error Checking
+    time_stored = lastupdate.read()
+    if time_stored == time_on_gs:
+        return 0
+    else:
+        return 1
+    lastupdate.close()
+
+def update_lastupdate(time_on_gs):
+    """
+    """
+    lastupdate = open(LAST_UPDATE, FILE_WRITE_MODE)
+    # TODO: Error checking
+    lastupdate.write(time_on_gs)
+    lastupdate.close()
+
+def update_lastcheck(current_time_str):
+    """
+    """
+    lastcheck = open(LAST_CHECK, FILE_WRITE_MODE)
+    # TODO: Error checking
+    lastcheck.write(current_time_str + CHAR_NEWLINE)
+    lastcheck.close()
+
+def send_email():
+    """
+    """
+    a = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    a.starttls()
+    a.login(MAIL_LOGIN, MAIL_PW)
     try:
-        interval = int(raw_input('Please specify the checking interval (in minute):'))
-        if interval == 0:
-            raise ValueError
-    except ValueError:
-        print 'Invalid interval. Program aborts.'
-        return
+        a.sendmail(SENDER, RECEIVERS, MSG)
+    except SMTPException:
+        print STR_ERR_SEND_EMAIL
+
+
+def main():
+    current_time_str = get_current_time_str()
+    soup = get_webpage()
+    time_on_gs = find_update(soup)
+    if check_diff(time_on_gs):
+        print STR_UPDATE, current_time_str
+        update_lastupdate(time_on_gs)
+        send_email()
+    else:
+        print STR_NO_UPDATE, current_time_str
+    update_lastcheck(current_time_str)
+
 
 if __name__ == '__main__':
     main()
