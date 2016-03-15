46916e1f785f64cd2d4178725a30a2c6c0e9cb4e
JokeyChen <jokey.chen@gmail.com>
2016-03-15 15:01:46 -0700

    Add helper method to setup

--- setup.py
+++ setup.py
@@ -1,24 +1,33 @@
 from json import dump
 
-def main():
-    f = open('ref/data', 'w')
-    mydict = {'url': 'SPECIFY GRADE SOURCE URL HERE',
-    'sender': 'SPECIFY EMAIL SENDER HERE',
-    'receivers': ['SPECIFY EMAIL RECEIVERS HERE'],
-    'smtp_server': 'SPECIFY THE SMTP SERVER HERE. DO NOT FORGET ITS PORT BELOW',
-    'smtp_port': 587,
-    'mail_login': 'SPECIFY LOGIN FOR SMTP',
-    'mail_pw': 'SPECIFY PASSWORD FOR SMTP HERE',
-    'msg': """From: John Doe <johndoe@xxx.com>
-    To: John Doe <johndoe@yyy.com>
-    Subject: GradeSource Update
+MY_DICT = {'url': 'SPECIFY GRADE SOURCE URL HERE',
+'sender': 'SPECIFY EMAIL SENDER HERE',
+'receivers': ['SPECIFY EMAIL RECEIVERS HERE'],
+'smtp_server': 'SPECIFY THE SMTP SERVER HERE. DO NOT FORGET ITS PORT BELOW',
+'smtp_port': 587,
+'mail_login': 'SPECIFY LOGIN FOR SMTP',
+'mail_pw': 'SPECIFY PASSWORD FOR SMTP HERE',
+'msg': """From: John Doe <johndoe@xxx.com>
+To: John Doe <johndoe@yyy.com>
+Subject: GradeSource Update
 
-    GradeSource Update Detected!
-    FEEL FREE TO MODIFY THE EMAIL MESSAGE HERE
-    """
-    }
-    dump(mydict, f)
+GradeSource Update Detected!
+FEEL FREE TO MODIFY THE EMAIL MESSAGE HERE
+"""
+}
+
+def create_data():
+    f = open('ref/data', 'w')
+    dump(MY_DICT, f)
     f.close()
+
+def create_lastupdate():
+    f = open('ref/lastupdate', 'w')
+    f.close()
+
+def main():
+    create_data()
+    create_lastupdate()
 
 if __name__ == '__main__':
     main()
